5fb96bbdac223f4489aded30556eb472d8ed36eb
“getromanin” <romanh99@gmail.com>
2018-11-27 23:20:51 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+// serve our index.html file, use express static middleware function
+app.use(express.static('public'));
+app.listen(process.env.PORT || 8080);
