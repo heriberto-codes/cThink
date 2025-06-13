99bfa914368ae785c4e3f4db16b8eb75dab1c6b6
hroman-codes <romanh99@gmail.com>
2025-06-13 16:40:29 -0400

    remove old files

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-
-// serve our index.html file, use express static middleware function
-app.use(express.static('public'));
-app.listen(process.env.PORT || 8080); 
